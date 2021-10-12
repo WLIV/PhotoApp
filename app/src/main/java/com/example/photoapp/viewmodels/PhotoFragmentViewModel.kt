d53f60acf87671841d34e5445a7f1360dd0d1ea5
WLIV <55149172+WLIV@users.noreply.github.com>
2021-10-12 17:04:51 +0300

    Using Fragments

---
+++ app/src/main/java/com/example/photoapp/viewmodels/PhotoFragmentViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.photoapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.photoapp.interfaces.ImageRepository
+import com.example.photoapp.repository.ImageLinkStorage
+
+class PhotoFragmentViewModel : ViewModel(){
+
+
+    private val repository: ImageRepository = ImageLinkStorage()
+
+
+    private var liveDataListUrls = MutableLiveData(
+        repository.getImages(
+            (3..15).random()
+        )
+    )
+
+    fun getUrls() : LiveData<List<String>>{
+        return liveDataListUrls
+    }
+}
